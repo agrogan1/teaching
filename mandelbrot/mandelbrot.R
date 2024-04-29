444a755359d5e31407f19621658cc4f75a0f03da
Grogan-Kaylor <agrogan@UMICH.EDU>
2024-04-29 10:47:08 -0400

    first commit

---
+++ mandelbrot/mandelbrot.R
@@ -0,0 +1,46 @@
+library(mandelbrot)
+
+library(viridis)
+
+library(ggplot2)
+
+mb <- mandelbrot(xlim = c(-2, 2), 
+                 ylim = c(-2, 2), 
+                 resolution = 2000, 
+                 iterations = 500)
+
+cols <- mandelbrot_palette(RColorBrewer::brewer.pal(11, "Spectral"), 
+                           fold = FALSE)
+
+png("./mandelbrot/mandelbrot.png")
+
+plot(mb,
+     xlim = c(-1, .1),
+     ylim = c(-1, 1),
+     # transform = "inverse",
+     col = cols)
+
+dev.off()
+
+citation(package = "mandelbrot")
+
+# mydata <- as.data.frame(mb)
+
+# plot(mydata$x,
+#      mydata$y,
+#      col = mydata$value)
+
+# ggplot(mydata,
+#        aes(x = x,
+#            y = y)) +
+#   scale_color_viridis_c() +
+#   # geom_point(aes(color = value)) +
+#   geom_raster(aes(fill = value), interpolate = TRUE) +
+#   # geom_contour(aes(z = value)) +
+#   theme_void()
+# 
+# ggsave("mandelbrot.png",
+#        height = 7,
+#        width = 7)
+# 
+
